ae78b25af43f66b5b4c8655270ee657781cb8cc5
IjjazAhmad <ijjazahmad705@gmail.com>
2024-01-10 23:12:17 +0500

    'firstcommit'

---
+++ src/Components/Header/TopBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function TopBar() {
+  return (
+    <>
+      <header class="bg-white">
+        <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div class="flex lg:flex-1">
+            <p>Need help? Call us: (+98) 0234 456 789</p>
+          </div>
+        
+          <div class="lg:flex lg:flex-1 lg:justify-end">
+            <p>Our store</p>
+          </div>
+        </nav>
+       
+      </header>
+
+    </>
+  )
+}
